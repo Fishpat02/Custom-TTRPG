6d79ccba2627a559a743af8c7f1aa502831f77af
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2026-09-08 16:10:47 -0400

    Added data types for dice and rolls and added probability functions

--- src/ttrpg_testing/dice.py
+++ src/ttrpg_testing/dice.py
@@ -1,8 +1,49 @@
+from dataclasses import dataclass, field
 from enum import Flag, auto
+from functools import reduce
 from random import randint
 
 BANE_THRESHOLD: int = 3
 BOON_THRESHOLD: int = 7
+
+BASE_DICE_SIDES = 10
+
+
+def n_combinations(target: int, dice: int, sides: int) -> int:
+    combinations: int = 0
+
+    if (dice * sides) < target:
+        return combinations
+    elif dice == 2:
+        for s in range(sides):
+            side = s + 1
+            remaining = target - side
+
+            if remaining <= 0:
+                break
+
+            if remaining <= sides:
+                combinations += 1
+    elif dice > 2:
+        for s in range(sides):
+            side = s + 1
+            remaining = target - side
+
+            if remaining < dice - 1:
+                break
+
+            combinations += n_combinations(remaining, dice - 1, sides)
+
+    return combinations
+
+
+def sum_prob(target: int, dice: int, sides: int = BASE_DICE_SIDES) -> float:
+    probability_sum: float = 0.0
+
+    for t in range(target, (dice * sides) + 1):
+        probability_sum += n_combinations(t, dice, sides) / float(sides) ** dice
+
+    return probability_sum
 
 
 class DieType(Flag):
@@ -11,12 +52,27 @@ class DieType(Flag):
     BOON = auto()
 
 
+@dataclass
+class Die:
+    value: int
+    die_type: DieType
+
+
+@dataclass
+class Roll:
+    dice: list[Die]
+    value: int = field(init=False)
+
+    def __post_init__(self):
+        self.value = reduce(lambda a, b: a + b, [x.value for x in self.dice])
+
+
 class DiceRoller:
     @staticmethod
     def roll_dice(
         count: int = 1, advantage: bool = False, disadvantage: bool = False
-    ) -> list[tuple[int, DieType]]:
-        rolls: list[tuple[int, DieType]] = []
+    ) -> Roll:
+        rolls: list[Die] = []
         num_dice: int = count
 
         if advantage and disadvantage:
@@ -30,10 +86,10 @@ class DiceRoller:
         for _ in range(num_dice):
             roll: int = randint(1, 10)
             if roll <= BANE_THRESHOLD:
-                rolls.append((roll, DieType.BANE))
+                rolls.append(Die(roll, DieType.BANE))
             elif roll <= BOON_THRESHOLD:
-                rolls.append((roll, DieType.NEUTRAL))
+                rolls.append(Die(roll, DieType.NEUTRAL))
             else:
-                rolls.append((roll, DieType.BOON))
+                rolls.append(Die(roll, DieType.BOON))
 
-        return rolls
+        return Roll(rolls)
