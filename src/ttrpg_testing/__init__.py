18c768fcd3545a268247f42838ba6202f62c9f6f
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2026-09-08 20:16:15 -0400

    Updated probability trials

--- src/ttrpg_testing/__init__.py
+++ src/ttrpg_testing/__init__.py
@@ -11,24 +11,34 @@ class Trial:
 
 
 def main() -> None:
-    num_trials: int = 20
-
-    for i, v in enumerate(range(1, num_trials, 4)):
+    for i, v in enumerate([1, 5, 10, 15]):
         level = v
         num_dice = 1 + i
 
-        trial = Trial(15, True, DieAdvantage.NEUTRAL)
+        trials = [
+            ("Flat", Trial(15, False, DieAdvantage.NEUTRAL)),
+            ("Advantage", Trial(15, False, DieAdvantage.ADVANTAGE)),
+            ("Disadvantage", Trial(15, False, DieAdvantage.DISADVANTAGE)),
+            ("Proficient", Trial(15, True, DieAdvantage.NEUTRAL)),
+            ("Prof + Adv", Trial(15, True, DieAdvantage.ADVANTAGE)),
+            ("Prof + Dis", Trial(15, True, DieAdvantage.DISADVANTAGE)),
+        ]
 
-        if trial.proficient:
-            num_dice += 1
+        print(f"Probability to pass DC 15 roll at level {level}:")
 
-        if trial.advantage & DieAdvantage.ADVANTAGE:
-            num_dice += 1
-        elif trial.advantage & DieAdvantage.DISADVANTAGE:
-            num_dice -= 1 if num_dice > 1 else 0
+        for trial in trials:
+            trial_dice = num_dice
+            if trial[1].proficient:
+                trial_dice += 1
 
-        probability = sum_prob(15, num_dice)
+            if trial[1].advantage == DieAdvantage.ADVANTAGE:
+                trial_dice += 1
 
-        print(
-            f"Probability to pass DC 15 at level {level} rolling {num_dice} dice: {probability:.2%}"
-        )
+            if trial[1].advantage == DieAdvantage.DISADVANTAGE:
+                trial_dice -= 1 if trial_dice > 1 else 0
+
+            probability = sum_prob(trial[1].dc, trial_dice)
+
+            print(f"{trial[0]:>15}: {probability:.2%} ({trial_dice} dice)")
+
+        print()
