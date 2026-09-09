99a49cec32cab7f2dba97ca4d86d77a1edb6c255
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2026-09-09 00:25:16 -0400

    Added additional DC checks and critical % output

--- src/ttrpg_testing/__init__.py
+++ src/ttrpg_testing/__init__.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from .dice import DieAdvantage, sum_prob
+from .dice import DieAdvantage, sum_prob_max, sum_prob_min
 
 
 @dataclass
@@ -11,34 +11,46 @@ class Trial:
 
 
 def main() -> None:
-    for i, v in enumerate([1, 5, 10, 15]):
-        level = v
-        num_dice = 1 + i
+    for _, dc in enumerate([10, 15, 20, 25]):
+        for prof_cap, level in enumerate([1, 5, 10, 15]):
+            num_dice = 1
 
-        trials = [
-            ("Flat", Trial(15, False, DieAdvantage.NEUTRAL)),
-            ("Advantage", Trial(15, False, DieAdvantage.ADVANTAGE)),
-            ("Disadvantage", Trial(15, False, DieAdvantage.DISADVANTAGE)),
-            ("Proficient", Trial(15, True, DieAdvantage.NEUTRAL)),
-            ("Prof + Adv", Trial(15, True, DieAdvantage.ADVANTAGE)),
-            ("Prof + Dis", Trial(15, True, DieAdvantage.DISADVANTAGE)),
-        ]
+            trials = [
+                ("Flat", Trial(dc, False, DieAdvantage.NEUTRAL)),
+                ("Advantage", Trial(dc, False, DieAdvantage.ADVANTAGE)),
+                ("Disadvantage", Trial(dc, False, DieAdvantage.DISADVANTAGE)),
+                ("Proficient", Trial(dc, True, DieAdvantage.NEUTRAL)),
+                ("Prof + Adv", Trial(dc, True, DieAdvantage.ADVANTAGE)),
+                ("Prof + Dis", Trial(dc, True, DieAdvantage.DISADVANTAGE)),
+            ]
 
-        print(f"Probability to pass DC 15 roll at level {level}:")
+            print(f"Probability to pass DC {dc} roll at level {level}:")
 
-        for trial in trials:
-            trial_dice = num_dice
-            if trial[1].proficient:
-                trial_dice += 1
+            for trial in trials:
+                trial_dice = num_dice
+                if trial[1].proficient:
+                    trial_dice += 1 + prof_cap
 
-            if trial[1].advantage == DieAdvantage.ADVANTAGE:
-                trial_dice += 1
+                if trial[1].advantage == DieAdvantage.ADVANTAGE:
+                    trial_dice += 1
 
-            if trial[1].advantage == DieAdvantage.DISADVANTAGE:
-                trial_dice -= 1 if trial_dice > 1 else 0
+                if trial[1].advantage == DieAdvantage.DISADVANTAGE:
+                    trial_dice -= 1 if trial_dice > 1 else 0
 
-            probability = sum_prob(trial[1].dc, trial_dice)
+                probability = sum_prob_min(trial[1].dc, trial_dice)
+                crit_probability = sum_prob_min(trial[1].dc + 10, trial_dice) or 0.0
+                fail_probability = (
+                    sum_prob_max(trial[1].dc - 10, trial_dice) or 1.0
+                    if dc - 10 > 0
+                    else 0.0
+                )
 
-            print(f"{trial[0]:>15}: {probability:.2%} ({trial_dice} dice)")
+                print(
+                    f"{trial[0]:>15}: {probability:7.2%} [{crit_probability:7.2%} Critical Success] [{fail_probability:7.2%} Critical Failure] ({trial_dice} dice)"
+                )
 
-        print()
+            print()
+
+        print(
+            "------------------------------------------------------------------------------------------\n"
+        )
