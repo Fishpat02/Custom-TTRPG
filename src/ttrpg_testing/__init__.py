b73ebfa4e4205c1e5b88081fdd825e6234816e7b
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2026-09-20 12:41:18 -0400

    Migrated dataframe generation to __init__.py to facilitate re-use for scripts and notebooks.

---
+++ src/ttrpg_testing/__init__.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+
+import polars as pl
+
+from .dice import DieAdvantage, sum_prob_max, sum_prob_min
+
+
+@dataclass
+class Trial:
+    dc: int
+    proficient: bool
+    advantage: DieAdvantage
+
+
+def gen_dataframe() -> pl.DataFrame:
+    dcs = [10, 15, 20, 25]
+    levels = [1, 5, 10, 15]
+    roll_types = [
+        "Flat",
+        "Advantage",
+        "Disadvantage",
+        "Proficient",
+        "Prof + Adv",
+        "Prof + Dis",
+    ]
+
+    trials_data = []
+
+    for dc in dcs:
+        for l, level in enumerate(levels):
+            prof_cap = l + 1
+
+            trials = zip(
+                roll_types,
+                [
+                    Trial(dc, False, DieAdvantage.NEUTRAL),
+                    Trial(dc, False, DieAdvantage.ADVANTAGE),
+                    Trial(dc, False, DieAdvantage.DISADVANTAGE),
+                    Trial(dc, True, DieAdvantage.NEUTRAL),
+                    Trial(dc, True, DieAdvantage.ADVANTAGE),
+                    Trial(dc, True, DieAdvantage.DISADVANTAGE),
+                ],
+            )
+
+            for name, trial in trials:
+                trial_dice = 1
+                if trial.proficient:
+                    trial_dice += prof_cap
+
+                if trial.advantage == DieAdvantage.ADVANTAGE:
+                    trial_dice += 1
+
+                if trial.advantage == DieAdvantage.DISADVANTAGE:
+                    trial_dice -= 1 if trial_dice > 1 else 0
+
+                probability = sum_prob_min(trial.dc, trial_dice)
+                crit_probability = sum_prob_min(trial.dc + 10, trial_dice) or 0.0
+                fail_probability = (
+                    sum_prob_max(trial.dc - 10, trial_dice) or 1.0
+                    if dc - 10 > 0 and trial_dice < dc - 10
+                    else 0.0
+                )
+
+                trials_data += [
+                    [
+                        name,
+                        dc,
+                        level,
+                        trial_dice,
+                        probability,
+                        crit_probability,
+                        fail_probability,
+                    ]
+                ]
+
+    return pl.DataFrame(
+        trials_data,
+        orient="row",
+        schema={
+            "roll_type": pl.String,
+            "dc": pl.Int32,
+            "level": pl.Int32,
+            "num_dice": pl.Int32,
+            "probability": pl.Float32,
+            "crit_probability": pl.Float32,
+            "fail_probability": pl.Float32,
+        },
+    )
